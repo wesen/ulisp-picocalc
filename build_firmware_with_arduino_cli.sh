1d5a75ef1616c90019fd5a12a692998d78ddfdc8
Manuel Odendahl <wesen@ruinwesen.com>
2026-05-06 15:13:41 -0400

    Extract uLisp memory management module

--- build_firmware_with_arduino_cli.sh
+++ build_firmware_with_arduino_cli.sh
@@ -32,6 +32,7 @@ cp \
   "$SOURCE_DIR/ulisp_fwd_decls.h" \
   "$SOURCE_DIR/ulisp_entry.cpp" \
   "$SOURCE_DIR/ulisp_error.cpp" \
+  "$SOURCE_DIR/ulisp_memory.cpp" \
   "$SOURCE_DIR/repl_window.h" \
   "$SOURCE_DIR/repl_window.cpp" \
   "$SOURCE_DIR/Setup60_RP2040_ILI9488.h" \
